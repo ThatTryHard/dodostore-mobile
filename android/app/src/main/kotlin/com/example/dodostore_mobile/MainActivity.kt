90c3b75a345e5590d255181d384f06510272ef38
ThatTryHard <orlando.devito@ui.ac.id>
2024-11-05 11:09:37 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/dodostore_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dodostore_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:dodostore_mobile/menu.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.cyan)
+            .copyWith(secondary: Colors.grey[400]),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
